7c24f632c74a264be75f27d5aaa88c23de0fad16
helloitsjoe <jboyle617@gmail.com>
2021-07-09 08:02:12 -0400

    upgrade eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
   extends: 'helloitsjoe',
-  rules: {
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        labelAttributes: ['label'],
-        depth: 3,
-      },
-    ],
-  },
 };
